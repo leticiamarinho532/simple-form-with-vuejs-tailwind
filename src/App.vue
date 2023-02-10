9ca083b66c50c438407389d5a2b4530c3afe47e8
Leticia <leticia.marinho532@gmail.com>
2023-02-09 21:40:38 -0300

    feat: create vue project + form structure

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Form from './components/form/Form.vue'
+</script>
+
+<template>
+  <Form>
+    
+  </Form>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/form/Form.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<template>
+    <form action="">
+        <label>Form</label>
+
+        <label>First Name:</label>
+        <input type="text" placeholder="ex. Laura Smith" required/>
+
+        <label>Email:</label>
+        <input type="email" placeholder="ex. laura@email.com" required/>
+
+        <label>Phone</label>
+        <input type="tel" placeholder="ex. 5521999999999" required/>
+
+        <label>Birth Date:</label>
+        <input type="date" required/>
+
+        <label>Wants to Recive Newsletter?</label>
+        <div>
+            <label>Yes</label>
+            <input type="radio" name="newsletter"/>
+
+            <label>No</label>
+            <input type="radio" name="newsletter"/>
+        </div>
+
+        <button type="submit">Submmit</button>
+        <button type="reset">Reset</button>
+    </form>
+</template>
+
+<style scoped>
+</style>
